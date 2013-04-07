8616350557bdc0715c9b0e9c1d8c015719d1f356
narkisr <narkisr@gmail.com>
2013-04-07 19:32:01 +0300

    version ++

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swag "0.2.0"
+(defproject swag "0.2.1"
   :description "A DSL for documenting Compojure routes using Swagger spec"
   :url "https://github.com/narkisr/swag"
   :license {:name "Eclipse Public License"
