25bd4a6decaddfd125ef8083b49fbdde07b1a243
narkisr <narkisr@gmail.com>
2013-11-04 16:55:03 +0200

    minor version with setbase made optional

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swag "0.2.4"
+(defproject swag "0.2.5"
   :description "A DSL for documenting Compojure routes using Swagger spec"
   :url "https://github.com/narkisr/swag"
   :license {:name "Eclipse Public License"
